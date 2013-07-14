ef6a0b030367a1b830bc9d3a428639d037ae69f6
Daniel Stutzman <dtstutz@gmail.com>
2013-07-14 13:13:31 -0600

    Added dvc-sinatra.rb

---
+++ dvc-sinatra.rb
@@ -0,0 +1,32 @@
+require 'active_record'
+require 'logger'
+require 'sinatra'
+require 'yaml'
+
+# Enable sessions -- note that a web server restart will invalidate all cookies
+use Rack::Session::Cookie, secret: SecureRandom.hex
+
+# fix ConnectionTimeoutError
+after { ActiveRecord::Base.connection.close }
+
+# Comment this line to turn off Active Record SQL logging
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+if ENV['DATABASE_URL'] # for Heroku
+  db = URI.parse(ENV['DATABASE_URL'])
+  ActiveRecord::Base.establish_connection({
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    port:     db.port,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8',
+  })
+else # for local development
+  config = YAML.load_file('config/database.yml')
+  ActiveRecord::Base.establish_connection(config['development'])
+end
+
+# Load models from models directory, like Rails
+Dir['./models/*.rb'].each { |file| require file }
