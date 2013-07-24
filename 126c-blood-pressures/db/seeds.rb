a65ad02f6605f6d2134857e6bba24ec4db48f95e
Daniel Stutzman <dtstutz@gmail.com>
2013-07-23 22:17:36 -0600

    Added 126 bcf.

---
+++ 126c-blood-pressures/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Patient.create(systolic: 100, diastolic: 70)
+Patient.create(systolic: 110, diastolic: 75)
+Patient.create(systolic: 120, diastolic: 80)
+Patient.create(systolic: 125, diastolic: 85)
+Patient.create(systolic: 130, diastolic: 90)
+Patient.create(systolic: 135, diastolic: 95)
+Patient.create(systolic: 140, diastolic: 100)
