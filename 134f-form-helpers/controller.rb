ea65ce7165913bb256a07238e0723b9246d2f32d
Daniel Stutzman <dtstutz@gmail.com>
2013-07-28 15:35:57 -0600

    Added 134f.

---
+++ 134f-form-helpers/controller.rb
@@ -0,0 +1,14 @@
+require '../dvc-sinatra.rb'
+
+get "/" do
+  @email = Email.new
+  halt erb(:root)
+end
+
+post "/" do
+  email = Email.new
+  email.subject = params[:subject]
+  email.body    = params[:body]
+  email.save!
+  redirect "/"
+end
