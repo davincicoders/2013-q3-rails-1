104209154145dcd65719bf32fb5a48d218187398
Daniel Stutzman <dtstutz@gmail.com>
2013-07-28 16:40:57 -0600

    Added 135f-edit-blood-pressure.

---
+++ 135f-edit-blood-pressures/controller.rb
@@ -0,0 +1,19 @@
+require '../dvc-sinatra.rb'
+
+get "/" do
+  @patients = Patient.all
+  halt erb(:index)
+end
+
+get "/patients/:id" do
+  @patient = Patient.find(params[:id])
+  halt erb(:edit)
+end
+
+post "/patients/:id" do
+  @patient           = Patient.find(params[:id])
+  @patient.systolic  = params[:systolic]
+  @patient.diastolic = params[:diastolic]
+  @patient.save!
+  redirect "/patients/#{@patient.id}"
+end
