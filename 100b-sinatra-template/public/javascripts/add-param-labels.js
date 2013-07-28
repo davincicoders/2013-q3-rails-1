ce339076be97f84876c630151ad3d232c3d7c849
Daniel Stutzman <dtstutz@gmail.com>
2013-07-28 17:18:06 -0600

    Make add-param-labels.js into symlinks so I can easily update the code.

--- 100b-sinatra-template/public/javascripts/add-param-labels.js
+++ 100b-sinatra-template/public/javascripts/add-param-labels.js
@@ -8,24 +8,27 @@
   function update_param_label($input) {
     if ($input.attr('type') === 'radio' && $input.is(':checked') ||
         $input.attr('type') !== 'radio') {
+
       var selector = 'div[data-for-name="' + $($input).attr('name') + '"]';
       $(selector).remove();
+
       var val_quoted = "&quot;" + $($input).val() + "&quot;";
       if ($input.attr('type') === 'checkbox' && !$input.is(':checked')) {
         val_quoted = "nil";
       }
-      $($input).before(
-        "<div class='param-label' data-for-name='" +
-        $($input).attr('name') +
-        "'>&#8600; params[:" +
-        $($input).attr('name') +
-        "]==" + val_quoted + "</div>");
+
+      var name = $input.attr('name');
+      if (name !== '') {
+        $($input).before(
+          "<div class='param-label' data-for-name='" +
+          name + "'>&#8600; params[:" + name + "]==" + val_quoted + "</div>"
+        );
+      }
     }
   }
 
   function jquery_is_ready($) {
-    $(document).ready(function() {
-      $('head').append("<style>\n" +
+    $('head').append("<style>\n" +
     "    .hidden-field-marker {\n" +
     "      color: red;\n" +
     "      font-size: 8pt;\n" +
@@ -51,39 +54,51 @@
     "    }\n" +
     "</style>");
 
-      if (false) { // disable labeling of <form> tag and action
-        $('form').each(function(i) {
-          var form = $(this);
-          add_form_param_label(form);
-        });
-      }
+    if (false) { // disable labeling of <form> tag and action
+      $('form').each(function(i) {
+        var form = $(this);
+        add_form_param_label(form);
+      });
+    }
 
-      $('input, select, button, textarea').each(function(i) {
-        var input = $(this);
+    $('input, select, button, textarea').each(function(i) {
+      var input = $(this);
+      update_param_label(input);
+      input.change(function(event) {
         update_param_label(input);
-        input.change(function(event) {
-          update_param_label(input);
-        });
       });
+    });
 
-      $('input[type=hidden]').each(function(i) {
-        var input = $(this);
-        input.after("<span class='hidden-field-marker'>(hidden field)</span>");
-      });
+    $('input[type=hidden]').each(function(i) {
+      var input = $(this);
+      input.after("<span class='hidden-field-marker'>(hidden field)</span>");
     });
   }
 
-  // inject jquery if it doesn't exist
-  if (typeof($) == 'undefined') {
-    var script = document.createElement("script");
-    script.src =
-      "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js";
-    script.onload = script.onreadystatechange = function() {
+  function load() {
+    // inject jquery if it doesn't exist
+    if (typeof($) == 'undefined') {
+      var script = document.createElement("script");
+      script.src =
+        "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js";
+      script.onload = script.onreadystatechange = function() {
+        jquery_is_ready($);
+      };
+      document.head.appendChild(script);
+    } else {
       jquery_is_ready($);
-    };
-    document.head.appendChild(script);
-console.log('here');
-  } else {
-    jquery_is_ready($);
+    }
+  }
+
+  function add_button() {
+    var button = document.createElement('input');
+    button.type = 'button';
+    button.value = 'Label form params';
+    button.onclick = load;
+    document.body.appendChild(button);
+  }
+
+  if (window.addEventListener) {
+    window.addEventListener('load', add_button, false);
   }
 })();
