8919c1b12b6af6984a99d86c10d53daaa872f45a
Daniel Stutzman <dtstutz@gmail.com>
2013-07-28 22:54:25 -0600

    Added 137f.

---
+++ 137f-only-two-records/controller.rb
@@ -0,0 +1,32 @@
+require '../dvc-sinatra.rb'
+
+get "/" do
+  @bracelet_lines = BraceletLine.all
+  halt erb(:root)
+end
+
+get "/line1" do
+  @bracelet_line = BraceletLine.find(1)
+  halt erb(:edit)
+end
+
+post "/line1" do
+  line           = BraceletLine.find(1)
+  line.font_size = params[:font_size]
+  line.text      = params[:text]
+  line.save!
+  redirect "/"
+end
+
+get "/line2" do
+  @bracelet_line = BraceletLine.find(2)
+  halt erb(:edit)
+end
+
+post "/line2" do
+  line           = BraceletLine.find(2)
+  line.font_size = params[:font_size]
+  line.text      = params[:text]
+  line.save!
+  redirect "/"
+end
