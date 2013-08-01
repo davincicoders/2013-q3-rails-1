9a286ab3fc981151b08fd946e742575a3b56d456
Daniel Stutzman <dtstutz@gmail.com>
2013-08-01 15:19:53 -0600

    Added 141 bcf.

---
+++ 141c-login-as-guest/controller.rb
@@ -0,0 +1,26 @@
+require '../dvc-sinatra.rb'
+
+get "/" do
+  halt erb(:login)
+end
+
+# TODO: Change this POST handler's code to handle the login as guest checkbox
+post "/" do
+  if params[:username] != "amy"
+    @error = "Unknown username"
+    @old_username = params[:username]
+    halt erb(:login)
+  end
+
+  if params[:password] != "123"
+    @error = "Unknown password"
+    @old_username = params[:username]
+    halt erb(:login)
+  end
+
+  redirect "/welcome"
+end
+
+get "/welcome" do
+  halt erb(:welcome)
+end
---
+++ 141f-login-as-guest/controller.rb
@@ -0,0 +1,29 @@
+require '../dvc-sinatra.rb'
+
+get "/" do
+  halt erb(:login)
+end
+
+post "/" do
+  if params[:login_as_guest] != nil
+    redirect "/welcome"
+  end
+
+  if params[:username] != "amy"
+    @error = "Unknown username"
+    @old_username = params[:username]
+    halt erb(:login)
+  end
+
+  if params[:password] != "123"
+    @error = "Unknown password"
+    @old_username = params[:username]
+    halt erb(:login)
+  end
+
+  redirect "/welcome"
+end
+
+get "/welcome" do
+  halt erb(:welcome)
+end
