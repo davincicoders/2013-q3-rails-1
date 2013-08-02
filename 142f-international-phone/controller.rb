a660743b6062f7aae2bf0e09be1121207347d671
Daniel Stutzman <dtstutz@gmail.com>
2013-08-01 22:22:32 -0600

    Added Cancel button to 142 so 'if' statement is needed.

--- 142f-international-phone/controller.rb
+++ 142f-international-phone/controller.rb
@@ -11,11 +11,15 @@ get "/:id" do
 end
 
 post "/:id" do
-  @phone                = Phone.find(params[:id])
-  @phone.number         = params[:number]
-  @phone.country_code   = params[:country_code]
-  @phone.phone_type     = params[:phone_type]
-  @phone.has_voice_mail = params[:has_voice_mail] == "on"
-  @phone.save!
-  redirect "/"
+  if params[:commit] == "Cancel"
+    redirect "/"
+  else
+    @phone                = Phone.find(params[:id])
+    @phone.number         = params[:number]
+    @phone.country_code   = params[:country_code]
+    @phone.phone_type     = params[:phone_type]
+    @phone.has_voice_mail = params[:has_voice_mail] == "on"
+    @phone.save!
+    redirect "/"
+  end
 end
