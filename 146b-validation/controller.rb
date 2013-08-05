c45b520901e28c78e820ea1b4c099ddaaba3cc47
Daniel Stutzman <dtstutz@gmail.com>
2013-08-04 19:23:41 -0600

    Added 146b-validation bcf.

---
+++ 146b-validation/controller.rb
@@ -0,0 +1,23 @@
+require '../dvc-sinatra.rb'
+
+get "/" do
+  redirect "/users"
+end
+
+get "/users" do
+  @users = User.order(:id).all
+  halt erb(:index)
+end
+
+get "/users/new" do
+  @user = User.new
+  halt erb(:new)
+end
+
+post "/users/new" do
+  @user = User.new
+  @user.username = params["username"]
+  @user.password = params["password"]
+  @user.save!
+  redirect "/users"
+end
