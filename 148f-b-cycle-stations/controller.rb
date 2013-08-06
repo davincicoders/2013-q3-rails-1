894a57f302d2400c2f7521e80f8e31ec26e0995e
Daniel Stutzman <dtstutz@gmail.com>
2013-08-06 14:17:37 -0600

    Demonstrations of where conditions.

--- 148f-b-cycle-stations/controller.rb
+++ 148f-b-cycle-stations/controller.rb
@@ -4,3 +4,21 @@ get "/" do
   @stations = Station.order(:station_name).page(params[:page]).all
   halt erb(:index)
 end
+
+get "/planned" do
+  @stations = Station.where(status: "planned"
+    ).order(:station_name).page(params[:page]).all
+  halt erb(:index)
+end
+
+get "/existing" do
+  @stations = Station.where(status: "existing"
+    ).order(:station_name).page(params[:page]).all
+  halt erb(:index)
+end
+
+get "/zip/:zip_code" do
+  @stations = Station.where(zip_code: params[:zip_code]
+    ).order(:station_name).page(params[:page]).all
+  halt erb(:index)
+end
