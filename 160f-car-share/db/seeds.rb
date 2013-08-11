74c37c4188cdef6edf798232c5d4b3302db1738a
Daniel Stutzman <dtstutz@gmail.com>
2013-08-11 17:27:10 -0600

    Start 160f with one reserved car.

--- 160f-car-share/db/seeds.rb
+++ 160f-car-share/db/seeds.rb
@@ -4,3 +4,7 @@ Car.create(color: "white", model: "Toyota Celica")
 
 Member.create(username: "jen", password: "123")
 Member.create(username: "ted", password: "456")
+
+car = Car.where(model: "VW Jetta").first
+car.reserving_member_id = Member.where(username: "jen").first.id
+car.save!
