beec70a12339a431154871581e1c7d906da5bf0a
Daniel Stutzman <dtstutz@gmail.com>
2013-08-29 13:47:25 -0600

    Added Ursula who works for no one.

--- 184f-people-and-companies/db/seeds.rb
+++ 184f-people-and-companies/db/seeds.rb
@@ -2,6 +2,7 @@ company1 = Company.create! name: "Microsoft"
 company2 = Company.create! name: "IBM"
 company3 = Company.create! name: "Sun"
 
-Person.create! name: "John", company: company1
-Person.create! name: "Jill", company: company2
-Person.create! name: "Jack", company: company2
+Person.create! name: "John",   company: company1
+Person.create! name: "Jill",   company: company2
+Person.create! name: "Jack",   company: company2
+Person.create! name: "Ursula", company: nil
