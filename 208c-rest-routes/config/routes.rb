8403c364d5aa1f77d397a9be897cb54c78b866ba
Daniel Stutzman <dtstutz@gmail.com>
2013-09-07 17:32:01 -0600

    Duplicated 208f to 208c.

---
+++ 208c-rest-routes/config/routes.rb
@@ -0,0 +1,11 @@
+App::Application.routes.draw do
+  # resources :ads
+  get    "/ads"          => "ads#index"
+  post   "/ads"          => "ads#create"
+  get    "/ads/new"      => "ads#new"
+  get    "/ads/:id/edit" => "ads#edit"
+  get    "/ads/:id"      => "ads#show"
+  patch  "/ads/:id"      => "ads#update"
+  put    "/ads/:id"      => "ads#update"
+  delete "/ads/:id"      => "ads#destroy"
+end
