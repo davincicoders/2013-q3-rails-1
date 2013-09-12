c53ddcebdbf766168c704e167c2f9a17a2c9ca28
Daniel Stutzman <dtstutz@gmail.com>
2013-09-12 16:32:56 -0600

    Finished visual look of 216f.

--- 216f-oz-books/db/seeds.rb
+++ 216f-oz-books/db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.create! \
+  cover_filename: "Wizard_title_page.jpg",
+  number: 1,
+  title: "The Wonderful Wizard of Oz",
+  year: 1900,
+  publisher: "George M. Hill Company",
+  summary: "Dorothy and her little dog, Toto, get swept into the Land of Oz by a cyclone. She meets a living Scarecrow, a man made entirely of tin, and a Cowardly Lion while trying to get to the Emerald City to see the great Wizard. Also reprinted by various publishers under the names The New Wizard of Oz and The Wizard of Oz with occasional minor changes in the text. It was originally written as a one-shot book."
+
+Book.create! \
+  cover_filename: "Marvelous_land_of_oz.jpg",
+  number: 2,
+  title: "The Marvelous Land of Oz",
+  year: "1904",
+  publisher: "Reilly & Britton",
+  summary: "A little boy, Tip, escapes from his evil guardian, the witch Mombi, with the help of a walking wooden figure with a jack-o'-lantern head named Jack Pumpkinhead (brought to life with the magic Powder of Life Tip stole from Mombi), as well as a living Sawhorse (created from the same powder). Tip ends up on an adventure with the Scarecrow and Tin Woodman. It later transpires that Tip is actually Princess Ozma, the rightful ruler of Oz, who was transformed by the evil Mombi."
+
+Book.create! \
+  cover_filename: "Ozbook03cover.jpg",
+  number: 3,
+  title: "Ozma of Oz",
+  year: 1907,
+  publisher: "Reilly & Britton",
+  summary: "While traveling to Australia with her Uncle Henry, Dorothy is swept overboard with a hen named Billina. They land in Ev, a country across the desert from Oz, and, together with new-found mechanical friend Tik-Tok, they must save Ev's royal family from the evil Nome King. With Princess Ozma's help, they finally return to Oz."
+
+Book.create! \
+  cover_filename: "Ozbook04cover.jpeg",
+  number: 4,
+  title: "Dorothy and the Wizard in Oz",
+  year: 1908,
+  publisher: "Reilly & Britton",
+  summary: "On her way back from Australia, Dorothy visits her cousin, Zeb, in California. They are soon swallowed up by an earthquake, along with Zeb's horse Jim and Dorothy's cat Eureka. The group soon meets up with the Wizard and all travel underground back to Oz."
+
+Book.create! \
+  cover_filename: "TheRoadToOz.jpg",
+  number: 5,
+  title: "The Road to Oz",
+  year: 1909,
+  publisher: "Reilly & Britton",
+  summary: "Dorothy meets the Shaggy Man, and while trying to find the road to Butterfield, they get lost on an enchanted road. As they travel they meet the rainbow's daughter, Polychrome, and a little boy, Button-Bright. They have all sorts of strange adventures on the way to Oz."
