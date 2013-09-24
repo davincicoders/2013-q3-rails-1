24be0c0ad6d38b165d9dfc592928e1f3ba150382
Daniel Stutzman <dtstutz@gmail.com>
2013-09-24 14:37:23 -0600

    Removed answer to 226c.

--- 226c-model-methods/app/models/person.rb
+++ 226c-model-methods/app/models/person.rb
@@ -2,22 +2,4 @@ class Person < ActiveRecord::Base
   validates :first_name,  presence: true
   validates :last_name,   presence: true
   validates :birth_date,  presence: true
-  validate :birth_date_not_future
-
-  def birth_date_not_future
-    if self.birth_date != nil
-      if self.birth_date > Date.today
-        errors.add :birth_date, "can't be in the future"
-      end
-    end
-  end
-
-  def age
-    (Date.today - self.birth_date).to_i / 365
-  end
-
-  def full_name
-    "#{self.first_name} #{self.last_name}"
-  end
-
 end
