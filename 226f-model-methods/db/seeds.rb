a912dcbb4feb15c4d846a7b82405d53b56576198
Daniel Stutzman <dtstutz@gmail.com>
2013-09-24 11:50:42 -0600

    Added Person model.

--- 226f-model-methods/db/seeds.rb
+++ 226f-model-methods/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Person.create!({
+  first_name: "Bill",
+  last_name:  "Smith",
+  birth_date: "1971-11-05",
+})
